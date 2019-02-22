dcfb8302708a568c7429634de555ac6e99274ccf
runoob <test@runoob.com>
2019-02-22 11:17:38 +0800

    修改文件

--- src/main/java/how2j/test/TestSocket.java
+++ src/main/java/how2j/test/TestSocket.java
@@ -1,12 +1,26 @@
 package how2j.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class TestSocket {
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws IOException {
 		InetAddress host = InetAddress.getLocalHost();
 		String ip = host.getHostAddress();
-		System.out.println(ip);
+		String ipRange = ip.substring(0, ip.lastIndexOf('.'));
+		System.out.println(ipRange);
+		
+		Process p = Runtime.getRuntime().exec("ping "+"192.168.2.106");
+		BufferedReader red = new BufferedReader(new InputStreamReader(p.getInputStream(),"GBK"));
+		String line = null;
+		StringBuilder sb = new StringBuilder();
+		while((line=red.readLine())!=null) {
+			if(line.length()!=0) {
+				sb.append(line+"\r\n");
+			}
+		}
+		System.out.println(sb.toString());
 	}
 }
